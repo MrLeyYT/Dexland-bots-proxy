2d21de8517e18a016eae859be88c68169f1ab7af
MrLeyYT <166651634+MrLeyYT@users.noreply.github.com>
2024-05-01 19:34:37 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -36,10 +36,6 @@ const bot = mineflayer.createBot({ // Создание бота
   }
 });
 
-const { mineflayer: mineflayerViewer } = require('prismarine-viewer')
-bot.once('spawn', () => {
-  mineflayerViewer(bot, { port: 3007, firstPerson: true }) // port is the minecraft server port, if first person is false, you get a bird's-eye view
-});
 
 bot.once('login', () =>  {
  console.log('Зашел!')
