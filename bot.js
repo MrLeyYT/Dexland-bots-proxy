fcf40d2c3ec4719497fb0fd30bd873f082236ba7
MrLeyYT <166651634+MrLeyYT@users.noreply.github.com>
2024-05-03 22:55:43 +0300

    Add files via upload

--- bot.js
+++ bot.js
@@ -1,66 +1,61 @@
 const mineflayer = require('mineflayer'); // Установка MineFlayer
 const config = require('./config.json'); // Данные конфига
-const socks = require('socks').SocksClient // Установка прокси
-
-
-const random = Math.floor(Math.random() * 10000000000); // Генерация рандомных чисел после ника
-const botUsername = config.nickname + random; // Соединение ника из конфига и рандомных чисел
+const socks = require('socks').SocksClient; // Установка прокси
+const random = Math.floor(Math.random() * 100000); // Генерация рандомных чисел после ника
+// Генерация никнейма из n случайных букв
+const gen = () => { let nickname = ''; const alphabet = 'abcdefghijklmnopqrstuvwxyz'; for (let i = 0; i < config.nickname.number; i++) { nickname += alphabet.charAt(Math.floor(Math.random() * alphabet.length)); } return nickname; };
+const botUsername = generateNickname(); // Соединение ника из конфига и рандомных чисел
+const pass = generateNickname();
 const number = config.number + 8; // Номер слота нужного выживания, чтобы выбрать его в меню выбора выживания
-console.log(config)
-const bot = mineflayer.createBot({ // Создание бота
- username: config.nickname,
- version: config.version,
- skipValidation: false,
- host: config.ip,
- port: config.port,
- connect: (client) => { // Создание прокси подключения
-    socks.createConnection({
-      proxy: {
-        host: config.proxy.ip,
-        port: config.proxy.port,
-        type: config.proxy.type
-      },
-      command: 'connect',
-      destination: {
-        host: config.ip,
-        port: config.port
-      }
-    }, (err, info) => {
-      if (err) {
-        console.log(err);
-        return;
-      }
-      client.setSocket(info.socket);
-      client.emit('connect');
-    });
-  }
+let sleep=ms=>{
+  let d=Date.now();
+  while(Date.now()-d<ms){}
+};
+console.log(config);
+const bot = mineflayer.createBot({
+    username: botUsername,
+    version: config.version,
+    skipValidation: false,
+    host: config.ip,
+    port: config.port,
+    connect: (client) => { // Создание прокси подключения
+        socks.createConnection({
+            proxy: {
+                host: config.proxy.ip,
+                port: config.proxy.port,
+                type: config.proxy.type
+            },
+            command: 'connect',
+            destination: {
+                host: config.ip,
+                port: config.port
+            }
+        }, (err, info) => {
+            if (err) {
+                console.log(err);
+                return;
+            }
+            client.setSocket(info.socket);
+            client.emit('connect');
+        });
+    }
 });
 
-
-bot.once('login', () =>  {
- console.log('Зашел!')
+bot.once('login', () => {
+	console.log(`Бот успешно вошел с никнеймом: ${bot.username}`);
 });
 
 bot.on('spawn', () => {
-    bot.chat('/reg ' + config.password);
-	bot.chat('/surv'); // Открытие меню выбора выживания
-	bot.once('windowOpen', (window) => {
-	bot.clickWindow(number, 0, 0); // Выбор нужного выживания
+	sleep(1000);
+    bot.chat('/reg ' + pass);
+	sleep(1000);
+	bot.chat('/s' + config.number);
 	setInterval(() => {
-		bot.chat(config.message); 
-	}, config.cooldown); // Отправка сообщения раз в указанную задержку
-});
-});
-
-
-bot.on('json', (data) => {
-  // Handle JSON messages
-  console.log('Received JSON:', data);
+        bot.chat('!' + genMessage() + config.message + genMessage());
+    }, config.cooldown); // Отправка сообщения раз в указанную задержку
 });
 
 bot.on('kicked', (reason, loggedIn) => {
- console.log(`Bot has been kicked! Reason: ${reason}`);
- process.exit(0);
+    console.log(`Bot has been kicked! Reason: ${reason}`);
+    process.exit();
 }); // Вывод сообщения если бота кикнули
-
-
